a7ea4a9cf3fa9f9f05e4b4449cf3c5cc09e5e615
Trà My <tramy@MacBook-Pro-cua-Tra.local>
2022-05-28 18:15:44 +0700

    Thêm folder Thuchanh4.html

---
+++ Thuchanh4.html/Thuchanh4.js
@@ -0,0 +1,8 @@
+let wight = parseInt(prompt('Enter the width'))
+        let height = parseInt(prompt('Enter the height'))
+        let area = wight * height
+        document.write('Enter width ' + wight)
+        document.write('<br>')
+        document.write('Enter height ' + height)
+        document.write('<br>')
+        document.write('The area is ' + area)
